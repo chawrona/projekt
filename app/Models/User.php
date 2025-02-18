935172479204b5b218f3c9eb939f87049d32b402
Dawid <dawidchawrona@gmail.com>
2025-02-18 12:57:57 +0100

    fix(UserModel): added comment for HasFactory so php anylize works

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -33,12 +34,12 @@ use Illuminate\Notifications\Notifiable;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
  *
  * @mixin \Eloquent
- * @mixin \Illuminate\Database\Eloquent\Factories\HasFactory<\Database\Factories\UserFactory>
  */
 
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
     use Notifiable;
 
