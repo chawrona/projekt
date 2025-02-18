b9b84ebed93aadc5a9e5b86179c396a9eafeede4
Dawid <dawidchawrona@gmail.com>
2025-02-18 14:07:54 +0100

    fix(Seeders): added hasFactory to Post and Category models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    /** @use HasFactory<PostFactory> */
+    use HasFactory;
+
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'category_post')->withTrashed();
